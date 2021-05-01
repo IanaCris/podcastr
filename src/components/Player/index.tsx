6a5ffe370f42330dfcbd315977b9b23f2120ea9f
IanaCris <ianasousati@gmail.com>
2021-04-30 22:22:59 -0300

    next and previous audio

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -14,7 +14,11 @@ export function Player() {
     currentEpisodeIndex, 
     isPlaying, 
     togglePlay,
-    setPlayingState
+    setPlayingState,
+    playNext,
+    playPrevious,
+    hasNext,
+    hasPrevious,
   } = useContext(PlayerContext);
   
   useEffect(() => {
@@ -86,7 +90,11 @@ export function Player() {
           <button type="button" disabled={!episode}>
             <img src="/shuffle.svg" alt="Embaralhar"/>
           </button>
-          <button type="button" disabled={!episode}>
+          <button 
+            type="button" 
+            onClick={playPrevious}
+            disabled={!episode || !hasPrevious}
+          >
             <img src="/play-previous.svg" alt="Tocar anterior"/>
           </button>
           <button 
@@ -99,7 +107,11 @@ export function Player() {
             ?  <img src="/pause.svg" alt="Parar"/>
             : <img src="/play.svg" alt="Tocar"/> }            
           </button>
-          <button type="button" disabled={!episode}>
+          <button 
+            type="button" 
+            onClick={playNext}
+            disabled={!episode || !hasNext}
+          >
             <img src="/play-next.svg" alt="Tocar próxima"/>
           </button>
           <button type="button" disabled={!episode}>
