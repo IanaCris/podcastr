05a23bb874da408957e3d0ef327f977aa3b9b22e
IanaCris <ianasousati@gmail.com>
2021-05-01 23:23:35 -0300

    suffle audio

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -23,6 +23,7 @@ export function Player() {
     setPlayingState,
     playNext,
     playPrevious,
+    clearPlayerState,
     hasNext,
     hasPrevious,
   } = usePlayer();
@@ -45,6 +46,19 @@ export function Player() {
     audioRef.current.addEventListener('timeupdate', () => {
       setProgress(Math.floor(audioRef.current.currentTime));
     });
+  }
+
+  function handleSeek(amount: number) {
+    audioRef.current.currentTime = amount;
+    setProgress(amount);
+  }
+
+  function handleEpisodeEnded() {
+    if (hasNext) {
+      playNext();
+    } else {
+      clearPlayerState();
+    }
   }
 
   const episode = episodeList[currentEpisodeIndex];
@@ -81,6 +95,7 @@ export function Player() {
               <Slider 
                 max={episode.duration}
                 value={progress}
+                onChange={handleSeek}
                 trackStyle={{ backgroundColor: '#04d361' }}
                 railStyle={{ backgroundColor: '#168aad' }}
                 handleStyle={{ borderColor: '#04d361', borderWidth: 4 }}
@@ -98,6 +113,7 @@ export function Player() {
             ref={audioRef}
             loop={isLooping}
             autoPlay
+            onEnded={handleEpisodeEnded}
             onPlay={() => setPlayingState(true)}
             onPause={() => setPlayingState(false)}
             onLoadedMetadata={setupProgressListener}
